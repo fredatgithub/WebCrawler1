0c68e9d0795841e501cb0c37920a96c3af8bcc13
Freddy Juhel <domaine75@gmail.com>
2025-08-07 15:23:46 +0200

    Ajoutez des fichiers projet.

---
+++ WebCrawler1/FormMain.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WebCrawler1
+{
+  partial class FormMain
+  {
+    /// <summary>
+    /// Variable nécessaire au concepteur.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Nettoyage des ressources utilisées.
+    /// </summary>
+    /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Code généré par le Concepteur Windows Form
+
+    /// <summary>
+    /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+    /// le contenu de cette méthode avec l'éditeur de code.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.textBoxUrl = new System.Windows.Forms.TextBox();
+      this.buttonStart = new System.Windows.Forms.Button();
+      this.listBoxResult = new System.Windows.Forms.ListBox();
+      this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+      this.SuspendLayout();
+      // 
+      // textBoxUrl
+      // 
+      this.textBoxUrl.Location = new System.Drawing.Point(13, 24);
+      this.textBoxUrl.Name = "textBoxUrl";
+      this.textBoxUrl.Size = new System.Drawing.Size(700, 20);
+      this.textBoxUrl.TabIndex = 0;
+      // 
+      // buttonStart
+      // 
+      this.buttonStart.Location = new System.Drawing.Point(720, 20);
+      this.buttonStart.Name = "buttonStart";
+      this.buttonStart.Size = new System.Drawing.Size(75, 23);
+      this.buttonStart.TabIndex = 1;
+      this.buttonStart.Text = "Start";
+      this.buttonStart.UseVisualStyleBackColor = true;
+      this.buttonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+      // 
+      // listBoxResult
+      // 
+      this.listBoxResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+      this.listBoxResult.FormattingEnabled = true;
+      this.listBoxResult.Location = new System.Drawing.Point(572, 61);
+      this.listBoxResult.Name = "listBoxResult";
+      this.listBoxResult.Size = new System.Drawing.Size(223, 576);
+      this.listBoxResult.TabIndex = 2;
+      // 
+      // webBrowser1
+      // 
+      this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+      this.webBrowser1.Location = new System.Drawing.Point(13, 61);
+      this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+      this.webBrowser1.Name = "webBrowser1";
+      this.webBrowser1.Size = new System.Drawing.Size(540, 585);
+      this.webBrowser1.TabIndex = 3;
+      // 
+      // FormMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(823, 658);
+      this.Controls.Add(this.webBrowser1);
+      this.Controls.Add(this.listBoxResult);
+      this.Controls.Add(this.buttonStart);
+      this.Controls.Add(this.textBoxUrl);
+      this.Name = "FormMain";
+      this.Text = "Web crawler";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox textBoxUrl;
+    private System.Windows.Forms.Button buttonStart;
+    private System.Windows.Forms.ListBox listBoxResult;
+    private System.Windows.Forms.WebBrowser webBrowser1;
+  }
+}
+
---
+++ WebCrawler1/FormMain.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+
+namespace WebCrawler1
+{
+  public partial class FormMain: Form
+  {
+    public FormMain()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      listBoxResult.Items.Clear();
+      textBoxUrl.Text = "https://github.com/anthropics"; // Default URL for testing
+    }
+
+    private async void ButtonStart_Click(object sender, EventArgs e)
+    {
+      string url = textBoxUrl.Text;
+      if (!string.IsNullOrEmpty(url))
+      {
+        webBrowser1.Navigate(url);
+        List<string> links = await CrawlWebsite(url);
+        listBoxResult.Items.Clear();
+        foreach (var link in links)
+        {
+          listBoxResult.Items.Add(link);
+        }
+      }
+    }
+
+    private static async Task<List<string>> CrawlWebsite(string url)
+    {
+      List<string> links = new List<string>();
+      using (HttpClient client = new HttpClient())
+      {
+        try
+        {
+          var response = await client.GetStringAsync(url);
+          var htmlDoc = new HtmlDocument();
+          htmlDoc.LoadHtml(response);
+
+          foreach (var link in htmlDoc.DocumentNode.SelectNodes("//a[@href]"))
+          {
+            string hrefValue = link.GetAttributeValue("href", string.Empty);
+            if (!string.IsNullOrEmpty(hrefValue))
+            {
+              links.Add(hrefValue);
+            }
+          }
+        }
+        catch (Exception exception)
+        {
+          MessageBox.Show($"Erreur lors du crawling : {exception.Message}");
+        }
+      }
+
+      return links;
+    }
+  }
+}
